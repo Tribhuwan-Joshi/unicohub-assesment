a57c3bb951c662bc9205633ebc913858ea849d17
Tribhuwan <mathincosmos@gmail.com>
2023-07-08 21:01:46 +0530

    --amend

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="https://tribhuwan-joshi.github.io/unicohub-page">
       <App />
     </BrowserRouter>
   </React.StrictMode>
