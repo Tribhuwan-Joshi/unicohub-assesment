72610295c3300041a26b09c75d544f12e21badf0
Tribhuwan <mathincosmos@gmail.com>
2023-07-08 19:01:10 +0530

    style users card

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -12,8 +12,8 @@ function Users() {
           mode: "cors",
         });
         const res = await data.json();
-        setUsers(res.users.slice(0, 10));
-        // setUsers(res.slice(0,10));
+        setUsers(res.users.slice(0, 20));
+        console.log(res.users.slice(0, 1));
       } catch (err) {
         console.log(err);
       }
@@ -26,10 +26,26 @@ function Users() {
       <h1 className="font-light underline underline-offset-4 decoration-1 text-4xl">
         Our Users Data
       </h1>
-      <div className="min-w-[300px]  h-max lg:grid-cols-[repeat(4,minmax(200px,1fr))] flex-shrink-0 p-4  grid  grid-cols-[repeat(auto-fit,minmax(150px,1fr))] grid-rows-[repeat(2,minmax(200px,1fr))]  gap-6">
+      <div className="min-w-[300px]  h-max lg:grid-cols-[repeat(4,minmax(200px,1fr))] flex-shrink-0 p-4  grid  grid-cols-[repeat(auto-fit,minmax(150px,1fr))]   gap-8">
         {users.map((user) => (
-          <div key={user.id} className="bg-red-300">
+          <div
+            key={user.id}
+            className="p-2  rounded-md bg-gradient-to-br from-purple-700 to-red-500 border"
+          >
             <p>{user.firstName + " " + user.lastName}</p>
+            <img src={user.image} alt="image" />
+            <div className="flex mt-5 justify-center items-center">
+              <p>Age - </p>
+              <p>{user.age}</p>
+            </div>
+            <div className="flex justify-center items-center">
+              <p>Height - </p>
+              <p>{user.height}</p>
+            </div>
+            <div className="flex justify-center items-center">
+              <p>mac - </p>
+              <p>{user.macAddress}</p>
+            </div>
           </div>
         ))}
       </div>
