68d540f82fc88d0284d33820ddb7dff4cb962763
Tribhuwan <mathincosmos@gmail.com>
2023-07-08 20:52:20 +0530

    --amend

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Header from "./components/Header/Header";
 import Users from "./components/Users/Users";
 import Main from "./components/Main";
 import { Route, Routes } from "react-router-dom";
-import "@test"
 import "./index.css";
 
 function App() {
